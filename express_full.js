dfc603aa5716e9fc62c029b5ee9322bc6bb1afb6
kindcow <support@kindcow.finance>
2021-03-13 12:39:24 +0700

    limiter

--- express_full.js
+++ express_full.js
@@ -10,6 +10,22 @@ var timeout = require('connect-timeout');
 
 app.use(timeout('5s'));
 
+const rateLimit = require("express-rate-limit");
+
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+  windowMs: 1 * 30 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+//  apply to all requests
+app.use(limiter);
+
+
+
 //const helmet = require("helmet");
 //app.use(helmet());
 
