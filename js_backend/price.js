861aa528aad5dc74bf6f41e1dfa3c5d09f3e02e4
kindcow <support@kindcow.finance>
2021-03-28 08:34:55 +0700

    price-feeds

--- js_backend/price.js
+++ js_backend/price.js
@@ -54,6 +54,8 @@ setInterval(function(){
         WALLET.getrate("0x3b3213e8f78ed08bfc0c5640f730e9f0861967f1","KIND_BUSD",8,18);
         WALLET.getrate("0x0d38be02d322648eb895c6e52d8dc89a6e491c2c","ORE_KIND",10,8);
         WALLET.getrate("0xdC6C130299E53ACD2CC2D291fa10552CA2198a6b","WATCH_BNB",18,18);
+        WALLET.getrate("0x233242524229B8CEA887645746C8849577F88Aa2","WBST_BUSD",18,18);
+        WALLET.getrate("0xE85323D9D8060F2b22d906cc794D02F3604Bb4cB","OGC_BUSD",8,18);
        
         setting.price_feed.forEach(element => {
             
@@ -66,6 +68,10 @@ setInterval(function(){
         price1['price']["KIND"] = price1['pair']['KIND_BUSD'];
         price1['price']["ORE"]  = price1['pair']['ORE_KIND']*price1['pair']['KIND_BUSD'];
         price1['price']["WATCH"]  = price1['pair']['WATCH_BNB']*price1['price']['BNB'];
+        price1['price']["BST"]  = price1['pair']['WBST_BUSD'];
+        price1['price']["WBST"]  = price1['pair']['WBST_BUSD'];
+        price1['price']["OGC"]  = price1['pair']['OGC_BUSD'];
+        
         
         // console.log(price1);
 
